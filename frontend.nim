cb9a3216f2a09b759384174e4a30913b51531908
PMunch <peterme@peterme.net>
2019-05-16 08:43:02 +0200

    Create new front-end in Karax

---
+++ frontend.nim
@@ -0,0 +1,235 @@
+include karax / prelude
+import karax / [vstyles, kdom, kajax]
+import jsffi except `&`
+import json
+import sugar
+
+import macros, strutils
+
+macro createAliases(tag: string, body: untyped): untyped =
+  result = newStmtList()
+  for classes in body:
+    assert classes.kind == nnkStrLit, "Input must be a list of string literals"
+    let tmplName = newIdentNode(classes.strVal.multiReplace([(" ", ""), ("-", "")]))
+    result.add quote do:
+      macro `tmplName`(rest: varargs[untyped]): untyped =
+        var
+          exprs: seq[NimNode]
+          classes = `classes`
+        for argument in rest:
+          if $argument[0] != "class":
+            exprs.add argument
+          else:
+            classes = classes & " " & argument[1].strVal
+
+        var ret = nnkCall.newTree(
+          newIdentNode("buildHtml"),
+          nnkCall.newTree(
+            newIdentNode(`tag`)
+          )
+        )
+        for expr in exprs:
+          ret[1].add expr
+        ret[1].add nnkExprEqExpr.newTree(
+            newIdentNode("class"),
+            newLit(classes)
+          )
+        ret
+
+createAliases("button"):
+  "main button"
+  "other button"
+
+createAliases("tdiv"):
+  "headerbar"
+  "mainarea"
+  "base column"
+  "grow content"
+  "bar"
+  "big editor"
+  "small column"
+
+type
+  CodeMirror = distinct Element
+  OutputKind = enum
+    Debug = "debug", Output = "output"
+  Regex = ref object of RootObj
+
+proc newCodeMirror(element: Element, config: js): CodeMirror {. importcpp: "CodeMirror(@)" .}
+proc setValue(cm: CodeMirror, value: kstring) {.importcpp: "#.setValue(@)".}
+proc getValue(cm: CodeMirror): kstring {.importcpp: "#.getValue()".}
+proc replace(str: kstring, r: Regex, cmd: proc (x: kstring): kstring): kstring {.importcpp: "#.replace(@)".}
+proc setHash(str: kstring) {.importcpp: "window.location.hash = #".}
+proc sanitize(str: kstring): kstring {.importcpp: "DOMPurify.sanitize(#)".}
+converter toRegex(r: kstring): Regex {.importcpp: "new RegExp(#, 'g')", noSideEffect.}
+
+var
+  output = Output
+  myCodeMirror: CodeMirror
+  outputText: array[Output.low..Output.high, string]
+  runningCode = false
+  awaitingShare = false
+  loadedIx = ""
+  showingTour = false
+  loadedTour = ""
+  currentSection = 0
+  totalSections = 0
+  tourContent = buildHtml:
+    text "Loading tour..."
+
+proc postRender(data: RouterData) =
+  if myCodeMirror.Element == nil:
+    myCodeMirror = newCodeMirror(kdom.getElementById("editor"), js{
+      mode: "nim".kstring,
+      value: "".kstring,
+      tabSize: 2,
+      lineNumbers: true,
+      theme: "dracula".kstring
+    })
+  if showingTour:
+    var tourContent = kdom.getElementById("tour")
+    let sections = tourContent.getElementsByTagName("section")
+    totalSections = sections.len
+    for idx, section in sections:
+      if idx != currentSection:
+        section.style.display = "none"
+      else:
+        section.style.display = "block"
+        let
+          headers = section.getElementsByTagName("h1")
+          codes = section.getElementsByTagName("code")
+        kdom.getElementById("sectionTitle").innerHtml = $(currentSection + 1) & "/" & $totalSections & ": " & (if headers.len > 0: $headers[0].innerHtml else: "No title")
+        if codes.len > 0:
+          myCodeMirror.setValue(codes[^1].innerHtml)
+          codes[^1].style.display = "none"
+
+proc switchOutput() =
+  output = case output:
+    of Debug: Output
+    of Output: Debug
+
+proc runCode() =
+  runningCode = true
+  proc cb(httpStatus: int, response: cstring) =
+    runningCode = false
+    if httpStatus == 200:
+      let jsonResponse = parseJson($response)
+      outputText[Output] = jsonResponse["log"].getStr.replace("\n", "<br/>")
+      outputText[Debug] = $(jsonResponse["compileLog"].getStr
+        .replace("\n", "<br/>")
+        .replace("Hint: ", "<span class=\"hint\">Hint: </span>")
+        .replace("Error: ", "<span class=\"error\">Error: </span>")
+        .replace("\\[[^\\]]*\\]".kstring, (x) => "<span class=\"brackets\">" & x & "</span>"))
+      if outputText[Output] != "<br/>":
+        output = Output
+      else:
+        output = Debug
+    else:
+      outputText[Debug] = "No reply from server:<br/>" & $httpStatus & ": " & $response
+      outputText[Output] = ""
+      output = Debug
+
+  let request = %*{"code": $myCodeMirror.getValue(), "compilationTarget": "c"}
+  ajaxPost("http://localhost:5000/compile", @[], $request, cb)
+
+proc shareIx() =
+  awaitingShare = true
+  proc cb(httpStatus: int, response: cstring) =
+    awaitingShare = false
+    if httpStatus == 200:
+      outputText[Output] = $response
+      outputText[Debug] = ""
+      output = Output
+      if not showingTour:
+        loadedIx = outputText[Output][13..16]
+        setHash("#ix=" & loadedIx)
+    else:
+      outputText[Debug] = "No reply from server:<br/>" & $httpStatus & ": " & $response
+      outputText[Output] = ""
+      output = Debug
+
+  let request = %*{"code": $myCodeMirror.getValue(), "compilationTarget": "c"}
+  ajaxPost("http://localhost:5000/ix", @[], $request, cb)
+
+proc loadIx(id: string) =
+  proc cb(httpStatus: int, response: cstring) =
+    if httpStatus == 200:
+      outputText[Output] = ""
+      outputText[Debug] = ""
+      output = Output
+      myCodeMirror.setValue(response)
+    else:
+      outputText[Debug] = "No reply from server:<br/>" & $httpStatus & ": " & $response
+      outputText[Output] = ""
+      output = Debug
+
+  ajaxGet("http://localhost:5000/ix/" & id, @[], cb)
+  loadedIx = id
+
+proc loadTour(id: string) =
+  proc cb(httpStatus: int, response: cstring) =
+    if httpStatus == 200:
+      outputText[Output] = ""
+      outputText[Debug] = ""
+      output = Output
+      var tourNodes = buildHtml:
+        verbatim response.sanitize
+      tourContent = tourNodes
+    else:
+      outputText[Debug] = "No reply from server:<br/>" & $httpStatus & ": " & $response
+      outputText[Output] = ""
+      output = Debug
+
+  ajaxGet("http://localhost:5000/tour/" & encodeUriComponent(id), @[], cb)
+  loadedTour = id
+  showingTour = true
+
+proc createDom(data: RouterData): VNode =
+  let strhash = $data.hashPart
+  if strhash.len > "#ix=".len:
+    if strhash[0..3] == "#ix=":
+      if loadedIx != strhash[4..^1]:
+        loadIx(strhash[4..^1])
+  if strhash.len > "#tour=".len:
+    if strhash[0..5] == "#tour=":
+      if loadedTour != strhash[6..^1]:
+        loadTour(strhash[6..^1])
+  result = buildHtml(tdiv):
+    headerbar:
+      img(src = "assets/logo.svg")
+      span: text "Playground"
+    mainarea:
+      if showingTour:
+        baseColumn:
+          growContent(id = "tour"):
+            tourContent
+          bar:
+            span(id = "sectionTitle"): text ""
+            otherButton(onclick = () => (currentSection = max(currentSection - 1, 0))):
+              text "Previous"
+            mainButton(onclick = () => (currentSection = min(currentSection + 1, totalSections - 1))):
+              text "Next"
+      baseColumn:
+        bigEditor(id = "editor", class = "monospace")
+        smallColumn:
+          bar:
+            if not awaitingShare:
+              otherButton(onclick = shareIx):
+                text "Share to ix"
+            else:
+              otherButton(class = "is-loading"):
+                text "Share to ix"
+            otherButton(onclick = switchOutput):
+              text "Showing: " & $output
+            if not runningCode:
+              mainButton(onclick = runCode):
+                text "Run!"
+            else:
+              mainButton(class = "is-loading"):
+                text "Run!"
+          growContent(class = "monospace"):
+            verbatim outputText[output]
+
+setRenderer createDom, "ROOT", postRender
+setForeignNodeId "tour"
+setForeignNodeId "editor"
